6e5c40810d3fafd4bd4003c86568668c7da9505e
zfletch <zfletch2@gmail.com>
2020-12-24 10:05:53 -0500

    update rss

--- lib/rss_generator.rb
+++ lib/rss_generator.rb
@@ -1,5 +1,7 @@
 require 'nokogiri'
+require 'time'
 require 'date'
+require 'cgi'
 
 class RssGenerator
   def initialize(courses)
@@ -26,17 +28,21 @@ class RssGenerator
             xml.link 'https://zfletch.github.io/yale-courses-rss/'
           end
           xml.language 'en-us'
-          xml.lastBuildDate Time.now
+          xml.lastBuildDate Time.now.rfc822
           xml.link 'https://zfletch.github.io/yale-courses-rss/rss.xml'
           xml.title 'Open Yale Courses RSS'
+          xml[:atom].link(href: 'https://zfletch.github.io/yale-courses-rss/rss.xml', rel: 'self', type: 'application/rss+xml')
           xml[:itunes].author 'Open Yale Courses'
           xml[:itunes].explicit 'no'
-          xml[:itunes].image 'https://zfletch.github.io/yale-courses-rss/logo.png'
+          xml[:itunes].image(href: 'https://zfletch.github.io/yale-courses-rss/logo.png')
           xml[:itunes].owner do
             xml[:itunes].name 'Zachary Fletcher'
             xml[:itunes].email 'open.yale.courses.rss@gmail.com'
           end
           xml[:itunes].summary 'RSS Feed of audio from Open Yale Courses'
+          xml[:itunes].category(text: 'Education') do
+            xml[:itunes].category(text: 'Courses')
+          end
           items(xml)
         end
       end
@@ -46,6 +52,7 @@ class RssGenerator
   def rss_attributes
     {
       'xmlns:itunes': 'http://www.itunes.com/dtds/podcast-1.0.dtd',
+      'xmlns:atom': 'http://www.w3.org/2005/Atom',
       version: '2.0',
     }
   end
@@ -57,13 +64,15 @@ class RssGenerator
       course[:lectures].each.with_index do |lecture, ii|
         next unless lecture[:mp3_path]
 
+        description = CGI.escapeHTML("#{course[:course_title]}: #{lecture[:name]}")
+
         xml.item do
           xml.title "#{course[:course_number]}: #{lecture[:shortname]}"
-          xml.description "#{course[:course_title]}: #{lecture[:name]}"
-          xml[:itunes].summary "#{course[:course_title]}: #{lecture[:name]}"
+          xml.description description
+          xml[:itunes].summary description
           xml.link lecture[:link]
           xml.enclosure(url: lecture[:mp3_path], type: 'audio/mpeg', length: lecture[:length])
-          xml.pubDate(start_date + ii)
+          xml.pubDate((start_date + ii).rfc822)
           xml[:itunes].author course[:professor]
           xml[:itunes].duration duration_string(lecture[:duration])
           xml[:itunes].explicit 'no'
